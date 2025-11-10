51213c425ccb106f9f6a50abdedad3cc6817f66b
Thilini Cooray <bhh501@myy.haaga-helia.fi>
2025-11-10 12:04:54 +0200

    Updated frontend app.js

--- solar-app/frontend/app.js
+++ solar-app/frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "http://51.20.142.29:5000";
 
 const input = document.getElementById("cityInput");
 const btn = document.getElementById("searchBtn");
